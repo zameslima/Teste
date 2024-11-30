e8f6d223969f605603ae89d2bd2c29b35ff04d2e
zameslima <zameslima@gmail.com>
2024-11-30 18:35:35 -0300

    Update sd.js

--- sd.js
+++ sd.js
@@ -7,37 +7,49 @@ function menuShow() {
     }
 }
 function abrir(){
-   document.getElementById("diz123").innerHTML=`<button onclick="fechar()"> X </button>    <object data="https://subwaysurf.io/subway-surfers.play" type="application/pdf">
+    document.getElementById("diz123").style.display = "flex";
+    document.getElementById("diz123").innerHTML=`<div id="mod">
+             </div> <button id="btn1" onclick="fechar()"> X </button> <object data="https://subwaysurf.io/subway-surfers.play" type="application/pdf">
         <p>Seu navegador não tem um plugin pra PDF</p>
     </object>`;
     
 }function abrir1(){
-    document.getElementById("diz123").innerHTML=` <button onclick="fechar()"> X </button> <object data="https://html5.gamemonetize.co/jyeazqa0x7ym5xcq22g9mgv74s5tift1/?gamedomain=jogos360.com.br" type="application/pdf">
+    document.getElementById("diz123").style.display = "flex";
+    document.getElementById("diz123").innerHTML=`<div id="mod">
+             </div> <button id="btn1" onclick="fechar()"> X </button> <object data="https://html5.gamemonetize.co/jyeazqa0x7ym5xcq22g9mgv74s5tift1/?gamedomain=jogos360.com.br" type="application/pdf">
          <p>Seu navegador não tem um plugin pra PDF</p>
      </object>`;
      
      
  }
  function abrir2(){
-    document.getElementById("diz123").innerHTML=`<button onclick="fechar()"> X </button> <object data="https://cdn.bubbleshooter.com/games/bubbleshooter-game/" type="application/pdf">
+    document.getElementById("diz123").style.display = "flex";
+    document.getElementById("diz123").innerHTML=`<div id="mod">
+             </div> <button id="btn1" onclick="fechar()"> X </button><object data="https://cdn.bubbleshooter.com/games/bubbleshooter-game/" type="application/pdf">
          <p>Seu navegador não tem um plugin pra PDF</p>
      </object>`;}
     function abrir3(){
-        document.getElementById("diz123").innerHTML=`<button onclick="fechar()"> X </button> <object data="https://game.gombis.com/html5/12/monkey-mart/" type="application/pdf">
+        document.getElementById("diz123").style.display = "flex";
+        document.getElementById("diz123").innerHTML=`<div id="mod">
+                 </div> <button id="btn1" onclick="fechar()"> X </button> <object data="https://game.gombis.com/html5/12/monkey-mart/" type="application/pdf">
          <p>Seu navegador não tem um plugin pra PDF</p>
      </object>`;
     }
     function abrir4(){
-        document.getElementById("diz123").innerHTML=`<button onclick="fechar()"> X </button> <object data="https://html5.gamedistribution.com/rvvASMiM/a55c9cc9c21e4fc683c8c6857f3d0c75/index.html?gdpr-targeting=1&gd_sdk_referrer_url=https%3A%2F%2Fgryfek.pl%2Fogien-i-woda-1-lesna-swiatynia&gd_zone_config=eyJwYXJlbnRVUkwiOiJodHRwczovL2dyeWZlay5wbC9vZ2llbi1pLXdvZGEtMS1sZXNuYS1zd2lhdHluaWEiLCJwYXJlbnREb21haW4iOiJncnlmZWsucGwiLCJ0b3BEb21haW4iOiJncnlmZWsucGwiLCJoYXNJbXByZXNzaW9uIjp0cnVlLCJsb2FkZXJFbmFibGVkIjp0cnVlLCJob3N0IjoiaHRtbDUuZ2FtZWRpc3RyaWJ1dGlvbi5jb20iLCJ2ZXJzaW9uIjoiMS41LjE3In0%253D" type="application/pdf">
+        document.getElementById("diz123").style.display = "flex";
+    document.getElementById("diz123").innerHTML=`<div id="mod">
+             </div> <button id="btn1" onclick="fechar()"> X </button> <object data="https://html5.gamedistribution.com/rvvASMiM/a55c9cc9c21e4fc683c8c6857f3d0c75/index.html?gdpr-targeting=1&gd_sdk_referrer_url=https%3A%2F%2Fgryfek.pl%2Fogien-i-woda-1-lesna-swiatynia&gd_zone_config=eyJwYXJlbnRVUkwiOiJodHRwczovL2dyeWZlay5wbC9vZ2llbi1pLXdvZGEtMS1sZXNuYS1zd2lhdHluaWEiLCJwYXJlbnREb21haW4iOiJncnlmZWsucGwiLCJ0b3BEb21haW4iOiJncnlmZWsucGwiLCJoYXNJbXByZXNzaW9uIjp0cnVlLCJsb2FkZXJFbmFibGVkIjp0cnVlLCJob3N0IjoiaHRtbDUuZ2FtZWRpc3RyaWJ1dGlvbi5jb20iLCJ2ZXJzaW9uIjoiMS41LjE3In0%253D" type="application/pdf">
          <p>Seu navegador não tem um plugin pra PDF</p>
      </object>`;
     }
     function abrir5(){
-        document.getElementById("diz123").innerHTML=`<button onclick="fechar()"> X </button> <object data="https://www.jogos360.com.br/ourgames/pac_man/index.html" type="application/pdf">
+        document.getElementById("diz123").style.display = "flex";
+        document.getElementById("diz123").innerHTML=`<div id="mod">
+                 </div> <button id="btn1" onclick="fechar()"> X </button> <object data="https://www.jogos360.com.br/ourgames/pac_man/index.html" type="application/pdf">
          <p>Seu navegador não tem um plugin pra PDF</p>
      </object>`;
     }
 function fechar(){
-    document.getElementById("diz123").innerHTML="";
+    document.getElementById("diz123").style.display = "none";
      
  } 
